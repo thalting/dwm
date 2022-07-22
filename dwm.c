e7c91ac847ef7cca2ca86a837ab95b861335840c
Hiltjo Posthuma <hiltjo@codemadness.org>
2022-07-22 09:18:52 +0200

    Revert "do not call signal-unsafe function inside sighanlder"
    This reverts commit 6613d9f9a1a5630bab30bc2b70bdc793977073ee.
    
    Discussed on the mailinglist:
    https://lists.suckless.org/hackers/2207/18405.html

--- dwm.c
+++ dwm.c
@@ -1592,8 +1592,6 @@ setup(void)
 	Atom utf8string;
 
 	/* clean up any zombies immediately */
-	if (signal(SIGCHLD, sigchld) == SIG_ERR)
-		die("can't install SIGCHLD handler:");
 	sigchld(0);
 
 	/* init screen */
@@ -1692,6 +1690,8 @@ showhide(Client *c)
 void
 sigchld(int unused)
 {
+	if (signal(SIGCHLD, sigchld) == SIG_ERR)
+		die("can't install SIGCHLD handler:");
 	while (0 < waitpid(-1, NULL, WNOHANG));
 }
 
